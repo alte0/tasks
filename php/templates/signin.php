e929316772121cf7b09f501a6556814d7656359a
alte0 <ctpagateji@yandex.ru>
2019-07-03 13:50:06 +0500

    var msgs

--- php/templates/signin.php
+++ php/templates/signin.php
@@ -1,8 +1,6 @@
 <form class="form form_auth" action="<?= $_SERVER['PHP_SELF'] ?>" method="post">
   <h3 class="form__title">Авторизация</h3>
-  <?php
-  showError($errors);
-  ?>
+  <?php showMsgs($msgs); ?>
   <div class="form__row form__row_content-column">
     <label for="login">Ваш логин:</label><input class="form__input" id="login" type="text" name="login" minlength="2" maxlength="20" required <?= $_POST["login"] ? "value=" . $_POST["login"] : "" ?> />
   </div>
