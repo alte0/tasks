bf8dd9415029d2b0440d8d81f0fc7f9cd3896625
alte0 <ctpagateji@yandex.ru>
2019-07-15 11:40:19 +0500

    rename

---
+++ html/src/js/components/flatpickr-pl.js
@@ -0,0 +1,17 @@
+import flatpickr from 'flatpickr'
+import { Russian } from 'flatpickr/dist/l10n/ru'
+
+const dateFlatpickr = document.body.querySelector(`.flatpickr`)
+const configFlatpickr = {
+  locale: Russian,
+  defaultDate: `today`,
+  mode: `range`,
+  dateFormat: `d.m.Y`,
+  minDate: `today`,
+  weekNumbers: true,
+  wrap: true
+}
+
+if (dateFlatpickr) {
+  flatpickr(dateFlatpickr, configFlatpickr)
+}
