4672cd78d931f5eface794077f73270aa06e23eb
alte0 <ctpagateji@yandex.ru>
2019-07-17 19:13:50 +0500

    ckeditor5-classic

--- html/src/js/components/editor.js
+++ html/src/js/components/editor.js
@@ -1,1 +1,24 @@
-// #textarea-text
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
+import '@ckeditor/ckeditor5-build-classic/build/translations/ru'
+
+const textarea = document.querySelector('#textarea-text')
+const config = {
+  removePlugins: ['ImageUpload'],
+  toolbar: ['Heading', 'bold', 'italic', '|', 'Link', 'bulletedList', 'numberedList', 'blockQuote', 'MediaEmbed', 'Undo', 'Redo'],
+  language: 'ru'
+}
+let editor
+
+ClassicEditor.create(textarea, config)
+  .then(newEditor => {
+    editor = newEditor
+    return true
+  })
+  .catch(error => {
+    console.error(error)
+  })
+
+document.querySelector('[type="submit"]').addEventListener('click', () => {
+  const editorData = editor.getData()
+  textarea.innerHTML = editorData
+})
