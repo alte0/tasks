8178daa213bc7b94afd9fce56693292e244ffb78
alte0 <ctpagateji@yandex.ru>
2019-07-31 11:14:52 +0500

    переписал на fetch

--- html/src/js/components/forms.js
+++ html/src/js/components/forms.js
@@ -6,35 +6,39 @@ const formTaskAdd = document.body.querySelector(`.form_task-add`)
 
 function submitOnFormTaskAdd (evt) {
   evt.preventDefault()
-  const xhr = new XMLHttpRequest()
   const method = `POST`
   const url = `/ajax/task-add.php`
-  const isAsync = true
   const formData = new FormData(formTaskAdd)
   formData.append('ajax', 'yes')
   formData.append('task-add', '')
-
-  xhr.open(method, url, isAsync)
-
-  xhr.onreadystatechange = function () {
-    if (this.readyState === 4 && this.status === 200) {
-      const response = JSON.parse(this.responseText)
-      showMessage(response.msgsType, response.textMsgs)
-      formTaskAdd.reset()
-      clearDataEditor()
-      clearDataFlatpickr()
-    }
-    if (this.readyState === 4 && this.status !== 200) {
-      showMessage(TypeMessage.ERROR, `Не удалось добавить задачу`, `Ошибка`)
-    }
-  }
 
   const inputDate = formTaskAdd.querySelector(`.form__date`)
   const selectUser = formTaskAdd.querySelector(`.form__list-users`)
   const taskTitle = formTaskAdd.querySelector(`.form__title-add`)
   const taskText = formTaskAdd.querySelector(`#textarea-text`)
+
   if (inputDate.value !== '' && selectUser.value !== '' && taskTitle.value !== '' && taskText.value !== '') {
-    xhr.send(formData)
+    fetch(url, {
+      method,
+      body: formData
+    })
+      .then(response => {
+        if (response.ok && response.status === 200) {
+          return response.json()
+        } else {
+          throw new Error(`Не удалось добавить задачу`)
+        }
+      })
+      .then(data => {
+        showMessage(data.msgsType, data.textMsgs[0])
+        formTaskAdd.reset()
+        clearDataEditor()
+        clearDataFlatpickr()
+        return false
+      })
+      .catch(error => {
+        showMessage(TypeMessage.ERROR, error)
+      })
   } else {
     showMessage(TypeMessage.WARNING, `Заполните все поля!`, `Внимание!`)
   }
