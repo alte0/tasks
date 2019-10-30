75c1a92b26e1f1bfe05cc5c685a92e91b0e5ff7b
alte0 <ctpagateji@yandex.ru>
2019-10-30 13:43:52 +0500

    добавлены линки

--- php/templates/layout.php
+++ php/templates/layout.php
@@ -6,6 +6,8 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
   <meta name="copyright" content="Задачи" />
   <title><?= clearStrDataTags($title) ?></title>
+  <link rel="dns-prefetch"/>
+  <link rel="preconnect"/>
   <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900&amp;display=swap&amp;subset=cyrillic" rel="stylesheet" />
   <link rel="stylesheet" href="css/style.css" />
 </head>
