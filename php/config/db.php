64e7eaeaf9cc88d623ff4ec69125c5c2f905a299
alte0 <ctpagateji@yandex.ru>
2019-11-01 14:22:21 +0500

    fix access db

--- php/config/db.php
+++ php/config/db.php
@@ -1,5 +1,7 @@
 <?php
-$DB_host = '127.0.0.1';
-$DB_login = "root";
-$DB_password = "";
-$DB_name = "tasks-db";
+return [
+  "urlDB" => "127.0.0.1",
+  "userDB" => "root",
+  "passwordDB" => "",
+  "nameDB" => "tasks-db"
+];
