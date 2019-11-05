d22face254544a604f3e573cbf249d5ec80ce733
alte0 <ctpagateji@yandex.ru>
2019-11-05 17:36:08 +0500

    php unit test

--- php/tests-phpunit/tests-case/tests.php
+++ php/tests-phpunit/tests-case/tests.php
@@ -1,101 +1,113 @@
 <?php
-require __DIR__ . "/../../lib/lib.inc.php";
+require __DIR__ . "/../../vars.php";
+require __DIR__ . "/../../helpers.php";
 use PHPUnit\Framework\TestCase;
 
 class StackTest extends TestCase
 {
-  public function testName() {
+  public function testTextLength() {
+    $askOne = 'Значение должно быть от 2 до 20 символов';
+    $askTwo = null;
     // мин 2 символа
-    $name = "1";
-    $this->assertSame(false, validName($name));
+    $text = "1";
+    $this->assertSame($askOne, validateLength(MIN_LENGTH_TEXT, MAX_LENGTH_TEXT, $text));
     // мин 2 символа
-    $name = "1w";
-    $this->assertSame(true, validName($name));
+    $text = "1w";
+    $this->assertSame($askTwo, validateLength(MIN_LENGTH_TEXT, MAX_LENGTH_TEXT, $text));
     // макс 20 символа
-    $name = "1q2w3e4r5t6y7u8i9o0p1";
-    $this->assertSame(false, validName($name));
+    $text = "1q2w3e4r5t6y7u8i9o0p";
+    $this->assertSame($askTwo, validateLength(MIN_LENGTH_TEXT, MAX_LENGTH_TEXT, $text));
+    // 21 символ
+    $text = "1q2w3e4r5t6y7u8i9o0p1";
+    $this->assertSame($askOne, validateLength(MIN_LENGTH_TEXT, MAX_LENGTH_TEXT, $text));
   }
 
-  public function testSurName() {
-    // мин 2 символа
-    $surname = "1";
-    $this->assertSame(false, validName($surname));
-    // мин 2 символа
-    $surname = "1w";
-    $this->assertSame(true, validName($surname));
-    // макс 20 символа
-    $surname = "1q2w3e4r5t6y7u8i9o0p1";
-    $this->assertSame(false, validName($surname));
+  public function testLoginRegex() {
+    $askOne = "Только латинские буквы и цифры, символы: '_', '-', первый символ обязательно буква";
+    $askTwo = null;
+    
+    $text = "1";
+    $this->assertSame($askOne, validateLoginRegex($text));
+    $text = "12";
+    $this->assertSame($askOne, validateLoginRegex($text));
+    $text = "f2";
+    $this->assertSame($askTwo, validateLoginRegex($text));
+    $text = "f2kguigwigbfiwf654488";
+    $this->assertSame($askTwo, validateLoginRegex($text));
+    $text = "кцпкпукп";
+    $this->assertSame($askOne, validateLoginRegex($text));
+    $text = "dfwкцпкпукп";
+    $this->assertSame($askOne, validateLoginRegex($text));
+    $text = "1dfwкцпкпукп";
+    $this->assertSame($askOne, validateLoginRegex($text));
+    $text = "f2_-F";
+    $this->assertSame($askTwo, validateLoginRegex($text));
   }
 
-  public function testPatronymic() {
-    // мин 2 символа
-    $patronymic = "1";
-    $this->assertSame(false, validPatronymic($patronymic));
-    // мин 2 символа
-    $patronymic = "1w";
-    $this->assertSame(true, validPatronymic($patronymic));
-    // макс 20 символа
-    $patronymic = "1q2w3e4r5t6y7u8i9o0p1";
-    $this->assertSame(false, validPatronymic($patronymic));
+  public function testPassworsRegex() {
+    $askOne = "Строчные и прописные латинские буквы, цифры, спецсимволы.";
+    $askTwo = null;
+
+    $text = "1";
+    $this->assertSame($askOne, validatePassworsRegex($text));
+    $text = "12";
+    $this->assertSame($askOne, validatePassworsRegex($text));
+    $text = "f2";
+    $this->assertSame($askOne, validatePassworsRegex($text));
+    $text = "f2kguigwigbfiwf654488";
+    $this->assertSame($askOne, validatePassworsRegex($text));
+    $text = "кцпкпукп";
+    $this->assertSame($askOne, validatePassworsRegex($text));
+    $text = "dfwкцпкпукп";
+    $this->assertSame($askOne, validatePassworsRegex($text));
+    $text = "1dfwкцпкпукп";
+    $this->assertSame($askOne, validatePassworsRegex($text));
+    $text = "f2_-F";
+    $this->assertSame($askOne, validatePassworsRegex($text));
+
+    $text = "hhd4H-_@#";
+    $this->assertSame($askTwo, validatePassworsRegex($text));
   }
 
-  public function testLogin() {
-    // мин 2 символа
-    $login = "1";
-    $this->assertSame(false, validLogin($login));
-    // мин 2 символа первый не цыфра
-    $login = "12";
-    $this->assertSame(false, validLogin($login));
-    // мин 2 символа первый буква
-    $login = "f2";
-    $this->assertSame(true, validLogin($login));
-    // макс 20 символов
-    $login = "f2kguigwigbfiwf654488";
-    $this->assertSame(false, validLogin($login));
-    // только латинскиие
-    $login = "кцпкпукп";
-    $this->assertSame(false, validLogin($login));
-    // только латинскиие
-    $login = "dfwкцпкпукп";
-    $this->assertSame(false, validLogin($login));
-    // только латинскиие, первый не цыфра
-    $login = "1dfwкцпкпукп";
-    $this->assertSame(false, validLogin($login));
+  public function testPassworsEqually() {
+    $askOne = "Пароли не совпадают!";
+    $askTwo = null;
+
+    $text = "1";
+    $text2 = "2";
+    $this->assertSame($askOne, validatePassworsEqually($text, $text2));
+    $text = "12";
+    $text2 = "2";
+    $this->assertSame($askOne, validatePassworsEqually($text, $text2));
+    $text = "666hhgy";
+    $text2 = "666hhgy";
+    $this->assertSame($askTwo, validatePassworsEqually($text, $text2));
   }
 
-    public function testPsw() {
-      // мин 6 символа
-      $password = "1q2w3";
-      $this->assertSame(false, validPwd($password));
-      // макс 20 символа
-      $password = "1q2w3e4r5t6y7u8i9o0p1";
-      $this->assertSame(false, validPwd($password));
-      // мин 6 символа. Строчные и прописные латинские буквы, цифры, спецсимволы.
-      $password = "1test1";
-      $this->assertSame(false, validPwd($password));
-      // мин 6 символа. Строчные и прописные латинские буквы, цифры, спецсимволы.
-      $password = "1Test1";
-      $this->assertSame(true, validPwd($password));
-      // мин 6 символа. Строчные и прописные латинские буквы, цифры, спецсимволы.
-      $password = "@Test@";
-      $this->assertSame(true, validPwd($password));
-      // мин 6 символа. Строчные и прописные латинские буквы, цифры, спецсимволы.
-      $password = "@test@";
-      $this->assertSame(false, validPwd($password));
-      // мин 6 символа. Строчные и прописные латинские буквы, цифры, спецсимволы.
-      $password = "@testлЛ@";
-      $this->assertSame(false, validPwd($password));
-  } 
+  public function testClearInt() {
+    $text = "1";
+    $this->assertSame(1, clearInt($text));
+    $text = "12";
+    $this->assertSame(12, clearInt($text));
+    $text = "666hhgy";
+    $this->assertSame(666, clearInt($text));
+  }
 
-    public function testPsws() {
-      // одинаковые пароли
-      $password = "1q2w31";
-      $password2 = "1q2w32";
-      $this->assertSame(false, equalPwds($password, $password2));
-      // одинаковые пароли
-      $password = "1q2w3";
-      $password2 = "1q2w3";
-      $this->assertSame(true, equalPwds($password, $password2));
-  } 
+  public function testClearStrDataTags() {
+    $text = "<p>ffsdfsd</p>";
+    $this->assertSame("ffsdfsd", clearStrDataTags($text));
+    $text = "<p>ffsdfsd</p>";
+    $this->assertSame($text, clearStrDataTags($text, '<p>'));
+    $text = "<p>ffsdfsd</p></b>";
+    $this->assertSame("<p>ffsdfsd</p>", clearStrDataTags($text, '<p>'));
+  }
+
+  public function testTransformsDate() {
+    $text = "30.12.2019";
+    $this->assertSame("2019.12.30", transformsDate($text));
+    $text = "30-12-2019";
+    $this->assertSame("2019.12.30", transformsDate($text));
+    $text = "12/30/2019";
+    $this->assertSame("2019.12.30", transformsDate($text));
+  }
 }
