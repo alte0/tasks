b63af3af4be2afe93397886402c5bc1c7fdb62c0
alte0 <ctpagateji@yandex.ru>
2019-11-09 21:49:08 +0500

    fix user-menu

--- php/templates/tasks.php
+++ php/templates/tasks.php
@@ -1,9 +1,15 @@
-<div class="user-menu">
-  <span class="user-menu__user"><?= clearStrDataTags($user) ?></span>
-  <a class="user-menu__link" href="/<?= $linkHref ?>"><?= $linkText ?></a>
-  <a class="user-menu__link" href="/add-task.php">Поставить задачу</a>
-  <a class="user-menu__link user-menu__logout" href="/logout.php">Выйти</a>
-</div>
+<ul class="user-menu">
+  <li class="user-menu__item"><?= clearStrDataTags($user) ?></li>
+  <li class="user-menu__item">
+    <a class="user-menu__link" href="/<?= $linkHref ?>"><?= $linkText ?></a>
+  </li>
+  <li class="user-menu__item">
+    <a class="user-menu__link" href="/add-task.php">Поставить задачу</a>
+  </li>
+  <li class="user-menu__item">
+    <a class="user-menu__link user-menu__logout" href="/logout.php">Выйти</a>
+  </li>
+</ul>
 <div class="tasks">
   <h2 class="tasks__title"><?= $title ?></h2>
   <?php if (empty($tasks)): ?>
