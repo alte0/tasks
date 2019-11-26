850c2ddd6e7f23382d3623265a5add3271edf75d
alte0 <ctpagateji@yandex.ru>
2019-11-26 15:42:33 +0500

    добавил в форме placeholder

--- php/templates/search.php
+++ php/templates/search.php
@@ -1,7 +1,7 @@
 <form class="form form_search" action="/search.php" method="get">
   <h3 class="form__title visually-hidden">Поиск по задачам.</h3>
   <div class="form__row form__row_search">
-    <input class="form__input form__input_search" type="search" name="search-field" value="<?= $_GET["search-field"] ?? "" ?>" />
+    <input class="form__input form__input_search" type="search" name="search-field" placeholder="Поиск по задачам" value="<?= $_GET["search-field"] ?? "" ?>" />
     <button class="form__button" type="submit">Найти</button>
   </div>
 </form>
