715e36bb7a7c37144c3a5e22a18c11c891ce8ca3
Максим <ctpagateji@yandex.ru>
2019-12-05 11:34:17 +0500

    fix style

--- php/templates/signin.php
+++ php/templates/signin.php
@@ -2,12 +2,12 @@
   <h3 class="form__title">Авторизация</h3>
   <div class="form__row form__row_content-column">
     <label for="login">Ваш логин:</label>
-    <input class="form__input" id="login" type="text" name="login"  value="<?= getPostVal("login") ?>" />
+    <input class="form__input" id="login" type="text" name="login"  minlength="2" maxlength="20" required="required" value="<?= getPostVal("login") ?>" />
     <?= !empty($errorsForm["login"]) ? "<div class=\"text-error\">{$errorsForm["login"]}</div>" : "" ?>
   </div>
   <div class="form__row form__row_content-column">
     <label for="password">Пароль:</label>
-    <input class="form__input" id="password" type="password" name="password"  value="<?= getPostVal("password") ?>" />
+    <input class="form__input" id="password" type="password" name="password" minlength="6" maxlength="20" required="required" value="<?= getPostVal("password") ?>" />
     <?= !empty($errorsForm["password"]) ? "<div class=\"text-error\">{$errorsForm["password"]}</div>" : "" ?>
   </div>
   <div class="form__row form__row_content-column">
