d063a761488d5bb7d2abe231cbdf6d826f899aa7
Максим <ctpagateji@yandex.ru>
2020-03-02 12:45:56 +0500

    переписал на функцию

--- spa-react/src/components/task/task.js
+++ spa-react/src/components/task/task.js
@@ -3,11 +3,10 @@ import PropTypes from "prop-types";
 import "./task.scss";
 import {hasDateExpired} from "../../helpers/helpers"
 
-export const Task = (props) => {
+const getTaskContent = (props) => {
     const {
         isShowDesc,
         isMore,
-        isTasks,
         task
     } = props;
     const {
@@ -19,54 +18,15 @@ export const Task = (props) => {
         author,
         executor,
         id
-        } = task;
-
-    if (isTasks) {
-            return (
-                <React.Fragment>
-                    <h3 className="task__title">{title}</h3>
-                    <div className="task__desc">
-                        {
-                            isShowDesc ?
-                            <p>{desc}</p>
-                                :
-                                ""
-                        }
-                    </div>
-                    <footer className="task__footer">
-                        <div className="task__date-start">Начало задачи: {dateStart}
-                        </div>
-                        <div className="task__date-end">Закончить задачу до: {dateEnd}
-                        </div>
-                        <div className="task__status">Статус: {status}
-                        </div>
-                        <div className="task__execute">
-                            <button>Выполнить задачу</button>
-                        </div>
-                        <div className="task__author">Назначил: {author}
-                        </div>
-                        <div className="task__executor">Испонитель: {executor}
-                        </div>
-                    </footer>
-                    {
-                        isMore ?
-                            <a
-                                className="link"
-                                data-id={id}
-                                onClick={props.handleClickMore}
-                                href={`/?taskId=${id}`}>Подробнее</a>
-                            : ''
-                    }
-                </React.Fragment>
-            )
-        }
+    } = task;
+    
     return (
-        <section className={`task ${hasDateExpired(dateEnd) ? `task_expired` : ``} ${isShowDesc ? `task_single` : ''}`}>
-            <h3 className="task__title">{title}</h3>
+        <React.Fragment>
+            <h3 className="task__title">${title}</h3>
             <div className="task__desc">
                 {
                     isShowDesc ?
-                    <p>{desc}</p>
+                        <p>{desc}</p>
                         :
                         ""
                 }
@@ -92,9 +52,32 @@ export const Task = (props) => {
                         className="link"
                         data-id={id}
                         onClick={props.handleClickMore}
-                        href={`/?taskId=${id}`}>Подробнее</a>
+                        href={`/? taskId = ${id}`}>Подробнее</a>
                     : ''
             }
+        </React.Fragment>
+    )
+}
+
+export const Task = (props) => {
+    const {
+        isShowDesc,
+        isTasks,
+        task
+    } = props;
+    const {
+        dateEnd
+    } = task;
+
+    if (isTasks) {
+            return (
+                getTaskContent(props)
+            )
+        }
+
+    return (
+        <section className={`task ${hasDateExpired(dateEnd) ? `task_expired` : ``} ${isShowDesc ? `task_single` : ''}`}>
+            {getTaskContent(props)}
         </section>
     )
 }
