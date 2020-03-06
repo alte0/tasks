c2e6ca33106704fa7d6a08fa16088b4c160d0698
Максим <ctpagateji@yandex.ru>
2020-03-06 18:38:53 +0500

    refactor data.js

--- spa-react/src/data/data.js
+++ spa-react/src/data/data.js
@@ -1,40 +1,29 @@
 /**
  * Получение моих задач
  */
-export const getMyTasks = async () => {
-  const response = await fetch('http://tasks.loc:80/ajax/get-my-tasks.php');
+export const fetchRequest = async (link) => {
+  const response = await fetch(link);
 
   return await response.json();
 };
+/**
+ * Получение моих задач
+ */
+export const getMyTasks = () => fetchRequest('http://tasks.loc:80/ajax/get-my-tasks.php');
+
 /**
  * Получение моих выполненых задач
  */
-export const getMyTasksDone = async () => {
-  const response = await fetch('http://tasks.loc:80/ajax/get-my-tasks-done.php');
-
-  return await response.json();
-};
+export const getMyTasksDone = () => fetchRequest('http://tasks.loc:80/ajax/get-my-tasks-done.php');
 /**
  * Получение задач поставленных мною
  */
-export const getDesignatedTasks = async () => {
-  const response = await fetch('http://tasks.loc:80/ajax/get-designated-tasks.php');
-
-  return await response.json();
-};
+export const getDesignatedTasks = () => fetchRequest('http://tasks.loc:80/ajax/get-designated-tasks.php');
 /**
  * Выполненные задачи другими
  */
-export const getDesignatedTasksDone = async () => {
-  const response = await fetch('http://tasks.loc:80/ajax/get-designated-tasks-done.php');
-
-  return await response.json();
-};
+export const getDesignatedTasksDone = () => fetchRequest('http://tasks.loc:80/ajax/get-designated-tasks-done.php');
 /**
  * Получение всех ользователей
  */
-export const getAllUsers = async () => {
-  const response = await fetch('http://tasks.loc:80/ajax/get-all-users.php');
-
-  return await response.json();
-};
+export const getAllUsers = () => fetchRequest('http://tasks.loc:80/ajax/get-all-users.php');
