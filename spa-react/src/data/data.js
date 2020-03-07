8124c72841c94441f4610aac953b3d8a4bb763a1
Максим <ctpagateji@yandex.ru>
2020-03-07 09:19:44 +0500

    refactoring

--- spa-react/src/data/data.js
+++ spa-react/src/data/data.js
@@ -2,28 +2,28 @@
  * Получение моих задач
  */
 export const fetchRequest = async (link) => {
-  const response = await fetch(link);
+  const response = await fetch(`http://tasks.loc:80/ajax/${link}`);
 
   return await response.json();
 };
 /**
  * Получение моих задач
  */
-export const getMyTasks = () => fetchRequest('http://tasks.loc:80/ajax/get-my-tasks.php');
+export const getMyTasks = () => fetchRequest('get-my-tasks.php');
 
 /**
  * Получение моих выполненых задач
  */
-export const getMyTasksDone = () => fetchRequest('http://tasks.loc:80/ajax/get-my-tasks-done.php');
+export const getMyTasksDone = () => fetchRequest('get-my-tasks-done.php');
 /**
  * Получение задач поставленных мною
  */
-export const getDesignatedTasks = () => fetchRequest('http://tasks.loc:80/ajax/get-designated-tasks.php');
+export const getDesignatedTasks = () => fetchRequest('get-designated-tasks.php');
 /**
  * Выполненные задачи другими
  */
-export const getDesignatedTasksDone = () => fetchRequest('http://tasks.loc:80/ajax/get-designated-tasks-done.php');
+export const getDesignatedTasksDone = () => fetchRequest('get-designated-tasks-done.php');
 /**
  * Получение всех ользователей
  */
-export const getAllUsers = () => fetchRequest('http://tasks.loc:80/ajax/get-all-users.php');
+export const getAllUsers = () => fetchRequest('get-all-users.php');
