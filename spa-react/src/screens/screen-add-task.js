e22766d3c8741ee5ef5cb53579799ffe2e61f63d
Максим <ctpagateji@yandex.ru>
2020-03-18 21:50:33 +0500

    clear

--- spa-react/src/screens/screen-add-task.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import FormAddTask from "../components/forms/form-add-task";
-
-const ScreenAddTask = (props) => {
-    return (
-        <FormAddTask changeActivePage={props.changeActivePage}/>
-    )
-};
-export default ScreenAddTask
--- spa-react/src/screens/screen-sign-up.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import FormSingUp from "../components/forms/form-sing-up";
-
-const ScreenSingUp = (props) => {
-    return (
-        <FormSingUp changeActivePage={props.changeActivePage}/>
-    )
-};
-export default ScreenSingUp
