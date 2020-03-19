0361c2273e444a52001b0b08ba725d570928166e
Максим <ctpagateji@yandex.ru>
2020-03-19 08:51:32 +0500

    meta tag

--- php/templates/layout.php
+++ php/templates/layout.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-  <meta name="copyright" content="Задачи" />
+  <meta name="description" content="Вэб сайт постановки задач" />
   <title><?= clearStrDataTags($title) ?></title>
   <link rel="dns-prefetch" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.googleapis.com" />
