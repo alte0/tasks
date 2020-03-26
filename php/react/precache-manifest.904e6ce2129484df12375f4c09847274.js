39f77cfb08d0d511a4b5f07934ff8caad980bb42
Максим <ctpagateji@yandex.ru>
2020-03-26 13:58:01 +0500

    react copy

---
+++ php/react/precache-manifest.904e6ce2129484df12375f4c09847274.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d01e2bd47d1c9482d13d205c4e4826b0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9e80264898a7d770f785",
+    "url": "./static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "cca0a75a345e22a6f9df",
+    "url": "./static/css/main.629dc5cc.chunk.css"
+  },
+  {
+    "revision": "9e80264898a7d770f785",
+    "url": "./static/js/2.54356701.chunk.js"
+  },
+  {
+    "revision": "07387c8408fbdbfbdecc0797f00eefd9",
+    "url": "./static/js/2.54356701.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cca0a75a345e22a6f9df",
+    "url": "./static/js/main.bc67a8e9.chunk.js"
+  },
+  {
+    "revision": "43ae00e8c71e27ece866",
+    "url": "./static/js/runtime-main.f5e9acab.js"
+  }
+]);
