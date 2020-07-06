2ed735836948eb4a037f3962cff5dea1b129c249
Максим <makc-dmv@yandex.ru>
2020-07-06 11:57:05 +0500

    base setting redux

--- spa-react/src/index.js
+++ spa-react/src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { applyMiddleware, createStore, compose } from 'redux'
+import { Provider } from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import rootReducer from './reducers';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.querySelector('.root'));
+const middlewareEnhancer = applyMiddleware(thunkMiddleware);
+const composedEnhancers = compose(middlewareEnhancer);
+const store = createStore(rootReducer, undefined, composedEnhancers);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.querySelector('.root')
+  );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
